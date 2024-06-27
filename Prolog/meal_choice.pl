d5ffe0eda6961969c27aca4201338ac225da43bd
LolloLauz <lorenzo.luzi@cooder.it>
2024-06-27 15:28:47 +0200

    fixed prolog and update the Onology

--- Prolog/meal_choice.pl
+++ Prolog/meal_choice.pl
@@ -1,4 +1,5 @@
 % Meal definitions
+
 meal(carnivorous, tagliatelle_fritte).
 meal(carnivorous, lasagna).
 meal(carnivorous, tortellini).
@@ -9,9 +10,11 @@ meal(vegetarian, parmigiana).
 meal(fish, spaghetti_allo_scoglio).
 
 % Type of food
+
 food_type(Type, Meal) :- meal(Type, Meal).
 
 % Calories level of food
+
 food_calories(Level, salad) :- between(0,2,Level).
 food_calories(Level, pizza_margherita) :- between(0,1,Level).
 food_calories(Level, tortellini) :- between(0,1,Level).
@@ -23,18 +26,22 @@ food_calories(0, parmigiana).
 food_calories(0, tiramisu).
 
 % Calories chosen for a meal
+
+
 calories_chosen(Level, Meal) :- food_calories(Level, Meal).
 
 % Allergies related to food
+
 food_allergies(gluten_free, salad).
 food_allergies(lactose_free, salad).
 food_allergies(lactose_free, tortellini).
 food_allergies(lactose_free, spaghetti_allo_scoglio).
 
-% Allergies for a given meal
+
 allergies(AllergiesType, Meal) :- food_allergies(AllergiesType, Meal).
 
 % Guest menu definition
+
 guest_menu(Type, CaloriesLevel, Allergy, Meals) :-
     findall(Meal, 
         ( (Type == none ; food_type(Type, Meal)), 
@@ -45,7 +52,7 @@ guest_menu(Type, CaloriesLevel, Allergy, Meals) :-
     (SortedMeals \= [] -> Meals = SortedMeals ; Meals = none).
 
 % Example queries
-% ?- guest_menu(carnivorous, 1, none, NumMeals, Meals).
-% ?- guest_menu(vegetarian, 0, none, NumMeals, Meals).
-% ?- guest_menu(fish, 1, lactose_free, NumMeals, Meals).
-% ?- guest_menu(vegetarian, 1, gluten_free, NumMeals, Meals).
+% ?- guest_menu(carnivorous, 1, none, Meals).
+% ?- guest_menu(vegetarian, 0, none, Meals).
+% ?- guest_menu(fish, 1, lactose_free, Meals).
+% ?- guest_menu(vegetarian, 1, gluten_free, Meals).
